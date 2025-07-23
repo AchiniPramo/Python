b0cfa8c98a39dc7548232957980358710e1ed0cd
AchiniPramo <achinipramodhya4@gmail.com>
2025-07-23 16:44:47 +0530

    Implement multiplication function and user input handling in hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+def multiplication(a, b):
+    return a * b
+
+breakpoint()
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+mul = multiplication(x, y)
+print(mul)
